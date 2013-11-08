adec4df4c01150f9d74cc5386a8aa720c028016a
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2013-11-08 14:53:30 +0100

    forgot events

--- plugins/gigs.py
+++ plugins/gigs.py
@@ -25,7 +25,7 @@ def gigs(inp, nick=None, say=None, me=None, msg=None, bot=None):
     if 'error' in r:
         return "Error: {}.".format(r["message"])
 
-    llimit=r["@attr"]["total"] if r["@attr"]["total"] < maxgigs else maxgigs
+    llimit=r["events"]["@attr"]["total"] if r["events"]["@attr"]["total"] < maxgigs else maxgigs
 
     if type(r) == dict and "event" in r["events"] and type(r["events"]["event"]) == list:
         me("will headbang at these "+ llimit + "gigs with "+ nick +":")
