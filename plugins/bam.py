5103cc84a3b1eca0e2895330d03280eec566bf61
redanthrax <gary@hazardbrick.com>
2013-11-10 20:58:19 -0800

    Bam, chuck norris facts with name replace

---
+++ plugins/bam.py
@@ -0,0 +1,25 @@
+from util import hook, http, timesince
+from datetime import datetime
+
+api_url = "http://api.icndb.com/jokes/random"
+
+@hook.command('bam', autohelp=False)
+@hook.command(autohelp=False)
+def bam(inp, nick='', db=None, bot=None, notice=None):
+
+    response = []
+
+    name = nick
+    if inp:
+        name = inp
+
+    if inp:
+        response = http.get_json(api_url, firstName=name, lastName="~")
+    else:
+        response = http.get_json(api_url, firstName=name, lastName="~")
+
+    if response["type"] == "success":
+        bam = response["value"]["joke"].replace("{} ~".format(name), "{}".format(name))
+        return bam
+    else:
+        return u"There was a problem, sorry {}".format(nick)
