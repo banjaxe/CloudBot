c0a609e093a36eb0e1eabbeea484ca5fe8e13771
redanthrax <gary@hazardbrick.com>
2013-11-10 21:07:03 -0800

    Added html parser for returned bam string

--- plugins/bam.py
+++ plugins/bam.py
@@ -1,5 +1,6 @@
 from util import hook, http, timesince
 from datetime import datetime
+import HTMLParser
 
 api_url = "http://api.icndb.com/jokes/random"
 
@@ -19,7 +20,9 @@ def bam(inp, nick='', db=None, bot=None, notice=None):
         response = http.get_json(api_url, firstName=name, lastName="~")
 
     if response["type"] == "success":
-        bam = response["value"]["joke"].replace("{} ~".format(name), "{}".format(name))
+        bam = response["value"]["joke"].replace(u"{} ~".format(name), u"{}".format(name))
+        htmlParser = HTMLParser.HTMLParser()
+        bam = htmlParser.unescape(bam)
         return bam
     else:
         return u"There was a problem, sorry {}".format(nick)
