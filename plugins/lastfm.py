baa4c0975e0b0e28fb4a6e91ed97ba03ec2f9d6b
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2013-11-13 10:29:25 +0100

    utf-8 in gigs

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -235,12 +235,12 @@ def gigs(inp, conn=None, bot=None,nick=None, chan=None):
     if type(r) == dict and "event" in r["events"] and type(r["events"]["event"]) == list:
         conn.send("PRIVMSG {} :\x01ACTION will headbang at these {} gigs with {}:\x01".format(chan,llimit,nick))
         for event in r["events"]["event"]:
-            cancelled=" [CANCELLED] "  if event["cancelled"] == "1" else ""
+            cancelled="[CANCELLED] "  if event["cancelled"] == "1" else ""
             if "headliner" in event["artists"]:
                 headliner = event["artists"]["headliner"]
                 event["artists"]["artist"].remove(headliner)
             else:
                 headliner="TBA"
-            conn.send(u"PRIVMSG {} :{}:{}{} ({},{}), headliner: \x02{}\x0f with {}".format(chan,event["startDate"],cancelled,event["venue"]["name"],event["venue"]["location"]["city"],event["venue"]["location"]["country"],headliner.encode('utf-8'),", ".join(event["artists"]["artist"]).encode('utf-8')))
+            conn.send(u"PRIVMSG {} :{}: {}{} ({}, {}), headliner: \x02{}\x0f with {}".format(chan,event["startDate"],cancelled,event["venue"]["name"],event["venue"]["location"]["city"],event["venue"]["location"]["country"],headliner,", ".join(event["artists"]["artist"])).encode('utf-8'))
     else:
-        conn.send(u"PRIVMSG {} :{}, No gigs for {} :(".format(chan,nick,inp))
+        conn.send(u"PRIVMSG {} :{}, No gigs for {} :(".format(chan,nick,inp).encode('utf-8'))
