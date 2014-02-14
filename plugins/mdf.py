a6812090be68ef3e15d7213fbc9ba403eb8928bb
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2014-02-14 13:59:31 +0100

    < sabyt> maybe make the delay between MDF cancelled and saying it's a joke a little bit longer

--- plugins/mdf.py
+++ plugins/mdf.py
@@ -13,5 +13,5 @@ def mdf(inp, conn=None, bot=None,nick=None, chan=None):
     # JOKE HUEHUEHUE
     # return "{} days before MDF 2014!".format(delta.days)
     conn.send(u"PRIVMSG {} :{}".format(chan, "MDF 2014 cancelled :("))
-    time.sleep(3)
+    time.sleep(10)
     conn.send(u"PRIVMSG {} :Just kidding {}. {} days before MDF 2014!".format(chan,nick,delta.days))
