bdf0c735993e25f97da0d0e0ee5d1fde3f377472
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2014-02-14 10:00:41 +0100

    final mdf

--- plugins/mdf.py
+++ plugins/mdf.py
@@ -1,16 +1,17 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 from util import hook, http, timesince
 from datetime import datetime
-
+import time
 
 @hook.command(autohelp=False)
-def mdf(inp):
+def mdf(inp, conn=None, bot=None,nick=None, chan=None):
     """mdf -- Displays countdown before Maryland Death Fest 2014
     """
 
     delta = datetime(2014, 05, 22) - datetime.now()
 
     # JOKE HUEHUEHUE
-    #return "MDF 2014 cancelled :("
-    #time.sleep(3)
-    return "{} days before MDF 2014!".format(delta.days)
+    # return "{} days before MDF 2014!".format(delta.days)
+    conn.send(u"PRIVMSG {} :{}".format(chan, "MDF 2014 cancelled :("))
+    time.sleep(3)
+    conn.send(u"PRIVMSG {} :Just kidding {}. {} days before MDF 2014!".format(chan,nick,delta.days))
