c75a2620cae8080ef5b9cebe109ab8b5132bb2cc
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2014-03-03 12:42:57 +0100

    ok joke is over

--- plugins/mdf.py
+++ plugins/mdf.py
@@ -11,7 +11,7 @@ def mdf(inp, conn=None, bot=None,nick=None, chan=None):
     delta = datetime(2014, 05, 22) - datetime.now()
 
     # JOKE HUEHUEHUE
-    # return "{} days before MDF 2014!".format(delta.days)
-    conn.send(u"PRIVMSG {} :{}".format(chan, "MDF 2014 cancelled :("))
-    time.sleep(10)
-    conn.send(u"PRIVMSG {} :Just kidding {}. {} days before MDF 2014!".format(chan,nick,delta.days))
+    return "{} days before MDF 2014!".format(delta.days)
+    #conn.send(u"PRIVMSG {} :{}".format(chan, "MDF 2014 cancelled :("))
+    #time.sleep(10)
+    #conn.send(u"PRIVMSG {} :Just kidding {}. {} days before MDF 2014!".format(chan,nick,delta.days))
