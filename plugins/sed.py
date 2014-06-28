0e596ce3fa3c3b444a7861e528a5983e620655d5
Gary Gagnon <gary@demon>
2014-06-28 02:58:37 -0700

    modified:   sed.py

--- plugins/sed.py
+++ plugins/sed.py
@@ -8,17 +8,22 @@ sedreg = (r'^s\/(.+)\/(.+)'
 matchany = (r'.+'
 	'.+', re.I|re.S)
 
-history = ''
+if 'history' not in globals():
+	global history
+
+history = []
 
 @hook.regex(*matchany)
-def saveHistory(match):
+def saveHistory(match, nick=''):
 	sedmatch = re.match(r's\/(.+)\/(.+)', match.group())
 	if(sedmatch == None):
-		global history
-		history = match.group()
+		history.insert(0, [nick, match.group()])
+		if len(history) > 10:
+			history.pop()
 
 @hook.regex(*sedreg)
-def doSed(match, db=None):
-	#WHYYYYYYYYYYYYYYY FUCKING REGEX
-	replaceString = "{}{}".format(match.group(2), match.group(3))
-	return "You meant {}.".format(history.replace(match.group(1), replaceString))
+def doSed(match):
+	for quote in history:
+		if match.group(1) in quote[1]:
+			replaceString = "{}{}".format(match.group(2), match.group(3))
+			return "{} meant to say '{}'".format(quote[0], quote[1].replace(match.group(1), replaceString))
