e5e3f283b391920f623ce60c564d0b881d7d18c3
banjaxe <zonesix@gmail.com>
2014-06-28 10:16:00 +0000

    refixing lastfm.py

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -5,8 +5,8 @@ import time
 
 api_url = "http://ws.audioscrobbler.com/2.0/?format=json"
 
+
 @hook.command('l', autohelp=False)
-@hook.command('np', autohelp=False)
 @hook.command(autohelp=False)
 def lastfm(inp, nick='', db=None, bot=None, notice=None):
     """lastfm [user] [dontsave] -- Displays the now playing (or last played)
@@ -140,3 +140,239 @@ def lastfm(inp, nick='', db=None, bot=None, notice=None):
         db.commit()
 
     return out
+
+
+
+@hook.command('compare', autohelp=False)
+@hook.command(autohelp=False)
+def compare(inp, nick='', db=None, bot=None, notice=None):
+    """compare [nick] -- Displays the comparison between users
+     from lastfm db."""
+    api_key = bot.config.get("api_keys", {}).get("lastfm")
+    if not api_key:
+        return "error: no api key set"
+
+    qUser = db.execute("select acc from lastfm where nick=lower(?)", (inp,)).fetchone()
+
+    if not qUser:
+        qUser = inp
+    else:
+        qUser = qUser[0]
+
+    nUser = db.execute("select acc from lastfm where nick=lower(?)", (nick,)).fetchone()
+
+    if not nUser:
+        nUser = nick
+    else:
+        nuser = nUser[0]
+
+
+    response = http.get_json(api_url, method="tasteometer.compare",
+                             api_key=api_key, type1="user", type2="user", value1=nuser, value2=qUser, limit=10)
+
+    if 'error' in response:
+        return "Error: {}.".format(response["message"])
+
+    score = round(float(response["comparison"]["result"]["score"]) * 100, 1)
+
+    if score == 0:
+        out = u"You and {} have no artists in common".format(inp)
+    else:
+        out = u"You and {} have {}% in common: artists(".format(qUser, score)
+
+        artists = []
+
+        for item in response["comparison"]["result"]["artists"]["artist"]:
+            artists.append(item["name"])
+
+        lastArtist = artists.pop()
+
+        for artist in artists:
+            out += u"{}, ".format(artist)
+        else:
+            out += u"{})".format(lastArtist)
+
+
+    return out
+
+@hook.command('b', autohelp=False)
+@hook.command(autohelp=False)
+def band(inp, nick='', db=None, bot=None, notice=None):
+    """band [band] -- Displays the band's informations
+     from lastfm db."""
+    api_key = bot.config.get("api_keys", {}).get("lastfm")
+    if not api_key:
+        return "error: no api key set"
+
+    r = http.get_json(api_url, method="artist.getInfo",
+                             api_key=api_key, artist=inp,autocorrect=1,limit=1)
+
+    if 'error' in r:
+        return "Error: {}.".format(r["message"])
+    out="No band named "+ inp
+    tags=[]
+    sims=[]
+
+    if type(r) == dict:
+        artist = r["artist"]
+        if type(artist) ==dict:
+            if type(artist["tags"]) == dict:
+                for tag in artist["tags"]["tag"]:
+                    tags.append(tag["name"])
+            else:
+                tags.append(u"No tags available")
+
+            if type(artist["similar"]) == dict:
+                if type(artist["similar"]["artist"]) == list:
+                    for sim in artist["similar"]["artist"]:
+                        sims.append(sim["name"])
+                elif type(artist["similar"]["artist"]) == dict:
+                    sims.append(artist["similar"]["artist"]["name"])
+                else:
+                    sims.append(u"No similar artists found")
+            else:
+                sims.append(u"No similar artists found")
+
+            placeformed=" ("+artist["bio"]["placeformed"] +")" if "placeformed" in artist["bio"] else ""
+            out = (artist["name"] + placeformed + " has "+ artist["stats"]["playcount"] + " plays by " + artist["stats"]["listeners"] + " listeners. Tags: " + ", ".join(tags) + ". Similar artists: " + ", ".join(sims) + ". More info on " + artist["url"]).encode("utf-8")
+    return out
+
+@hook.command('top', autohelp=False)
+@hook.command(autohelp=False)
+def top(inp, nick='', db=None, bot=None, notice=None):
+    """top -- Displays the top bands for [nick]
+     from lastfm db."""
+    api_key = bot.config.get("api_keys", {}).get("lastfm")
+    if not api_key:
+        return "error: no api key set"
+
+    if inp:
+        user = db.execute("select acc from lastfm where nick=lower(?)", (inp,)).fetchone()
+    else:
+        user = db.execute("select acc from lastfm where nick=lower(?)", (nick,)).fetchone()
+
+    if not user:
+        if not inp:
+            user = nick
+        else:
+            user = inp
+    else:
+        user = user[0]
+
+    response = http.get_json(api_url, method="user.gettopartists",
+                             api_key=api_key, user=user, period = '7day', limit=10)
+
+    if 'error' in response:
+        return "Error: {}.".format(response["message"])
+
+    topArtists = []
+    lastArtist = ''
+
+    out = u'Top 10 artists this week for {}: ('.format(user)
+
+    if len(response["topartists"]["artist"]) > 1:
+        for artist in response["topartists"]["artist"]:
+            topArtists.append(artist["name"])
+
+        lastArtist = topArtists.pop()
+    else:
+        lastArtist = response["topartists"]["artist"]["name"]
+
+
+    for artist in topArtists:
+        out += u"{}, ".format(artist)
+    else:
+        out += u"{})".format(lastArtist)
+
+    return out
+
+
+@hook.command('gi', autohelp=False)
+@hook.command(autohelp=False)
+def gigs(inp, conn=None, bot=None,nick=None, chan=None):
+    """gigs [band] -- Displays the band's gigs
+     from lastfm db."""
+
+    maxgigs=5
+    gig_counter=0
+
+    api_key = bot.config.get("api_keys", {}).get("lastfm")
+    if not api_key:
+        return "error: no api key set"
+
+    r = http.get_json(api_url, method="artist.getEvents",
+                             api_key=api_key, artist=inp,autocorrect=1,limit=20)
+
+    if 'error' in r:
+        return "Error: {}.".format(r["message"])
+
+    if type(r) == dict and "event" in r["events"] and type(r["events"]["event"]) == list:
+        conn.send("PRIVMSG {} :\x01ACTION will headbang at these gigs with {}:\x01".format(chan,nick))
+        for event in r["events"]["event"]:
+            if gig_counter == maxgigs:
+                break
+            others=""
+            cancelled="[CANCELLED] "  if event["cancelled"] == "1" else ""
+            if "headliner" in event["artists"]:
+                headliner = event["artists"]["headliner"]
+                if type(event["artists"]["artist"]) == list:
+                    event["artists"]["artist"].remove(headliner)
+                    others=" with {}".format(", ".join(event["artists"]["artist"]))
+            else:
+                headliner="TBA"
+            conn.send(u"PRIVMSG {} :{}: {}{} ({}, {}), headliner: \x02{}\x0f{}".format(chan,event["startDate"][:-9],cancelled,event["venue"]["name"],event["venue"]["location"]["city"],event["venue"]["location"]["country"],headliner,others))
+            gig_counter=gig_counter+1
+    else:
+        conn.send(u"PRIVMSG {} :{}, No gigs for {} :(".format(chan,nick,inp).encode('utf-8'))
+
+@hook.command(autohelp=False)
+def geogigs(inp, conn=None, bot=None,nick=None, chan=None):
+    """geogigs [location] -- Displays gigs in your area
+     from lastfm db."""
+
+    maxgigs=5
+    style='metal'
+    not_styles=['metalcore','nu metal','frenchcore']
+    gig_counter=0
+
+    api_key = bot.config.get("api_keys", {}).get("lastfm")
+    if not api_key:
+        return "error: no api key set"
+
+    r = http.get_json(api_url, method="geo.getEvents",
+                             api_key=api_key, location=inp,tag=style,limit=20)
+
+    if 'error' in r:
+        return "Error: {}.".format(r["message"])
+
+    if type(r) == dict and "event" in r["events"] and type(r["events"]["event"]) == list:
+        conn.send("PRIVMSG {} :\x01ACTION will headbang at these gigs with {}:\x01".format(chan,nick))
+        for event in r["events"]["event"]:
+            if gig_counter == maxgigs:
+                break
+            others=""
+            tags=""
+            cancelled="[CANCELLED] "  if event["cancelled"] == "1" else ""
+            if "headliner" in event["artists"]:
+                headliner = event["artists"]["headliner"]
+                if type(event["artists"]["artist"]) == list:
+                    event["artists"]["artist"].remove(headliner)
+                    others=" with {}".format(", ".join(event["artists"]["artist"]))
+            else:
+                headliner="TBA"
+            if type(event["tags"]["tag"]) == list:
+                # Get out if we meet a not_style tag
+                not_this_one="false"
+                for genre in not_styles:
+                    if genre in event["tags"]["tag"]:
+                        not_this_one="true"
+                        break
+                tags=", ".join(event["tags"]["tag"])
+            else:
+                tags=event["tags"]["tag"]
+
+            if not_this_one=="false":
+                conn.send(u"PRIVMSG {} :{}: {}{} ({}, {}), headliner: \x02{}\x0f{} ({})".format(chan,event["startDate"][:-9],cancelled,event["venue"]["name"],event["venue"]["location"]["city"],event["venue"]["location"]["country"],headliner,others,tags))
+                gig_counter=gig_counter+1
+    else:
+        conn.send(u"PRIVMSG {} :{}, No gigs for {} :(".format(chan,nick,inp))
