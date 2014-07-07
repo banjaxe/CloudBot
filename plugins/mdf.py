8343d42f67af75ccd435c93d1f980c517d7ed132
Gregoire Doumergue <gdoumergue@squidsolutions.com>
2014-07-07 11:02:44 +0200

    MDF 2015

--- plugins/mdf.py
+++ plugins/mdf.py
@@ -8,10 +8,10 @@ def mdf(inp, conn=None, bot=None,nick=None, chan=None):
     """mdf -- Displays countdown before Maryland Death Fest 2014
     """
 
-    delta = datetime(2014, 05, 22) - datetime.now()
+    delta = datetime(2015, 05, 21) - datetime.now()
 
     # JOKE HUEHUEHUE
-    return "{} days before MDF 2014!".format(delta.days)
+    return "{} days before MDF 2015!".format(delta.days)
     #conn.send(u"PRIVMSG {} :{}".format(chan, "MDF 2014 cancelled :("))
     #time.sleep(10)
     #conn.send(u"PRIVMSG {} :Just kidding {}. {} days before MDF 2014!".format(chan,nick,delta.days))
